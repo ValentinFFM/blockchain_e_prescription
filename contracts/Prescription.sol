e199095cc75b69b4c3819b5f5758c8284373749e
„Valentin <valentinmueller@live.de>
2020-12-10 22:27:23 +0100

    Small contract changes.

--- contracts/Prescription.sol
+++ contracts/Prescription.sol
@@ -4,6 +4,13 @@ pragma solidity >=0.4.21 <0.7.0;
 contract Prescription {
   string insurance = "";
 
+  // string insured_surname = "";
+  // string insured_name = "";
+  // string insured_street = "";
+  // string insured_street_number = "";
+  // uint insured_post_code = null;
+  // string insured_city = "";
+
   function getInsurance() public view returns (string memory) {
     return insurance;
   }
