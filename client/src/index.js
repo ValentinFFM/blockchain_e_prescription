0fbf8932e53b69aea320f08a56ad28c96c62b084
„Valentin <valentinmueller@live.de>
2020-12-15 09:35:51 +0100

    Creating new user contract

--- client/src/index.js
+++ client/src/index.js
@@ -8,9 +8,10 @@ import Login from './login'
 
 // Importing Bootstrap for styling
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './navigation';
 
 
-ReactDOM.render([<Login />], document.getElementById('root'));
+ReactDOM.render([<Navigation />], document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
