4e9aa3986add6884a25b7a21c4969029b688a422
„Valentin <valentinmueller@live.de>
2020-12-17 23:02:52 +0100

    Creating new smart contract with a new front-end page for User administration.

--- contracts/User.sol
+++ contracts/User.sol
@@ -3,4 +3,39 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract User {
 
+    struct Insured {
+        string surname;
+        string name;
+        string street;
+        string street_number;
+        uint post_code;
+        string city;
+        string birth_date;
+    }
+
+    Insured[] registeredInsured;
+
+    function addNewUser(string memory surname_, string memory name_, string memory street_, string memory street_number_, uint post_code_, string memory city_, string memory birth_date_)  public{
+
+        Insured memory insured_ = Insured(
+            {
+                surname: surname_,
+                name: name_,
+                street: street_,
+                street_number: street_number_,
+                post_code: post_code_,
+                city: city_,
+                birth_date : birth_date_
+            }
+        );
+
+        registeredInsured.push(insured_);
+    }
+
+    function checkUser() public returns (bool available) {
+
+        for user 
+
+    }
+
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Prescription = artifacts.require("./Prescription.sol");
+var User = artifacts.require("./User.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Prescription);
+  deployer.deploy(User);
 };
