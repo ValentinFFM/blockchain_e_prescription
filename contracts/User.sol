a85701c0bd5a8ca0657ce48921ea815cc437a774
„Valentin <valentinmueller@live.de>
2020-12-19 13:08:12 +0100

    Adding the physician struct to the user smart contract as well as writing some new functions.

--- contracts/User.sol
+++ contracts/User.sol
@@ -1,40 +1,66 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
 
+// Must be used to allow using structs as datatype within the function call
+pragma experimental ABIEncoderV2;
+
 contract User {
 
     struct Insured {
+        uint insurance_number;
+        uint insured_status;
+        
         string surname;
         string name;
+        string birth_date;
+
         string street;
         string street_number;
         uint post_code;
         string city;
-        string birth_date;
+    }
+
+    struct Physician {
+        uint business_number;
+
+        string job_title;
+        string surname;
+        string name;
+
+        string street;
+        string street_number;
+        uint post_code;
+        string city;
+
+        string telephone_number; 
     }
 
     mapping(string => Insured) public insureds;
-    string[] public InsuredAccounts;
-
-    function addNewUser(string memory surname_, string memory name_, string memory street_, string memory street_number_, uint post_code_, string memory city_, string memory birth_date_)  public{
-        
-        insureds[surname_] = Insured(surname_, name_, street_, street_number_, post_code_, city_, birth_date_);
-
-        // Insured storage insured = insureds[surname_];
-
-        // insured.surname = surname_;
-        // insured.name = name_;
-        // insured.street = street_;
-        // insured.street_number = street_number_;
-        // insured.post_code = post_code_;
-        // insured.city = city_;
-        // insured.birth_date = birth_date_;
+    mapping(uint => Physician) public physicians;
 
 
-        // InsuredAccounts.push(surname_);
+    function addNewInsured(string memory insured_number_, Insured memory insured_)  public{
+        insureds[insured_number_] = Insured(insured_.insurance_number, insured_.insured_status, insured_.surname, insured_.name, insured_.birth_date, insured_.street, insured_.street_number, insured_.post_code, insured_.city);
     }
 
-    function getUser(string memory surname_) public view returns (string memory, string memory, string memory, string memory) {
-        return (insureds[surname_].surname, insureds[surname_].name, insureds[surname_].street, insureds[surname_].street_number);
+
+    function addNewPhysician(uint physician_number_, Physician memory physician_) public{
+        physicians[physician_number_] = Physician(physician_.business_number, physician_.job_title, physician_.surname, physician_.name, physician_.street, physician_.street_number, physician_.post_code, physician_.city, physician_.telephone_number);
+    }
+
+    function checkInsuredExistance(string memory insured_number_) public view returns(bool){
+        if(insureds[insured_number_].insurance_number > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function checkPhysicianExistance(uint physician_number_) public view returns(bool){
+        if(physicians[physician_number_].business_number > 0){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
