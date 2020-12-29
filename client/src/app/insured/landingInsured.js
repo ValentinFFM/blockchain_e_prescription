58c869cc4b31a16e2dac047fc5a5481464501845
„Valentin <valentinmueller@live.de>
2020-12-29 17:05:10 +0100

    Add to last commit.

--- client/src/app/insured/landingInsured.js
+++ client/src/app/insured/landingInsured.js
@@ -2,19 +2,36 @@ import React, { Component } from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import PrescriptionList from './prescriptionList';
 
 class LandingInsured extends Component {
     render(){
         return(
-            <Navbar bg="dark" variant="dark" expand="lg">
-                <Navbar.Brand>E-Prescription</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                    </Nav>
-                    <Button href="/Logout" variant="outline-danger">Logout</Button>
-                </Navbar.Collapse>
-            </Navbar>
+            <>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Navbar.Brand>E-Prescription</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                        </Nav>
+                        <Button href="/Logout" variant="outline-danger">Logout</Button>
+                    </Navbar.Collapse>
+                </Navbar>
+
+                <Container fluid className="mt-5">
+                    <Row> 
+                        <Col xs={0} sm={1} md={3} lg={4}></Col>
+                        <Col>
+                            <h1>Aktuelle Rezepte</h1>
+                            <PrescriptionList />
+                        </Col>
+                        <Col xs={0} sm={1} md={3} lg={4}></Col>
+                    </Row>
+                </Container>
+            </>
         );
     }
 }
